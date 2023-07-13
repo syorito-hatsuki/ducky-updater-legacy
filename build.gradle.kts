307a10ec39d00a98265d23b01fc49744c7bb673c
syorito-hatsuki <enderdron3@gmail.com>
2023-07-13 16:42:58 +0300

    Added Kotlin support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("fabric-loom")
+    kotlin("jvm")
 }
 
 base {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,5 +8,8 @@ pluginManagement {
     plugins {
         val loomVersion: String by settings
         id("fabric-loom") version loomVersion
+
+        val kotlinVersion: String by System.getProperties()
+        kotlin("jvm") version kotlinVersion
     }
 }
